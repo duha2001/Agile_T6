4032813b7a34fbfbed43a2a930f7d4eede5cdc98
lehai4 <86140878+lehai4@users.noreply.github.com>
2022-03-21 23:35:37 +0700

    push a little

---
+++ AT-18/Cart.jsx
@@ -0,0 +1,66 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
+import { numberWithCommas, Helmet, Banner, Button, CartItem } from "../Common";
+const Cart = (props) => {
+  const { datas, getCartItemDetails } = props;
+  const carts = useSelector((state) => state.cart.carts);
+  const [cartProduct, setCartProduct] = useState([]);
+  const [totalCart, setTotalCart] = useState(0);
+  const handleOrder = () => {
+    toast.success(
+      "Cảm ơn bạn đã mua hàng. Vui lòng kiểm tra mail sau 3 ngày ^^"
+    );
+  };
+  useEffect(() => {
+    setCartProduct(getCartItemDetails(carts.slice(1)));
+    let result = carts.reduce((total, currentValue) => {
+      return Number(total) + currentValue.price;
+    }, 0);
+    setTotalCart(result);
+  }, [carts]);
+  return (
+    <Fragment>
+      <Helmet title="cart">
+        <Banner
+          img="https://magiamgialazada.vn/wp-content/uploads/2018/01/voucher-bi-mat-lazada-khuyen-mai-tet-am-lich-2018-truyen-nhan-sam-tet.png"
+          marginBottom={50}
+        />
+        <div className="cart">
+          <div className="cart__list">
+            {cartProduct.length ? (
+              cartProduct.map((item, index) => (
+                <CartItem key={index} item={item} />
+              ))
+            ) : (
+              <img
+                src="https://bizweb.dktcdn.net/100/368/179/themes/738982/assets/empty-cart.png?1609300798440"
+                alt=""
+              />
+            )}
+          </div>
+          <div className="cart__info">
+            <div className="cart__info__txt">
+              <p>Shopping Bag ({carts.slice(1).length})</p>
+              <div className="cart__info__txt__price">
+                <span>Thành tiền</span>
+                <span>{numberWithCommas(totalCart)}$</span>
+              </div>
+            </div>
+            <div className="cart__info__btn">
+              <Button size="block" onClick={handleOrder}>
+                Đặt hàng
+              </Button>
+              <Button size="block">
+                <Link to="/catelog">Tiếp tục mua hàng</Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Helmet>
+    </Fragment>
+  );
+};
+export default React.memo(Cart);
---
+++ AT-18/Login.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Helmet, Button } from "../Common";
+import GoogleButton from "react-google-button";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const refInputEmail = useRef(null);
+  const refInputPassword = useRef(null);
+  const navigate = useNavigate();
+  const signIn = () => signInWithEmailAndPassword(auth, email, password);
+  const handleSubmit = async (e) => {
+    setError("");
+    e.preventDefault();
+    if (email === "" || password === "") {
+      email === ""
+        ? refInputEmail.current.focus()
+        : refInputPassword.current.focus();
+    } else {
+      setError("");
+      try {
+        await signIn(auth, email, password);
+        navigate("/");
+      } catch (error) {
+        setError(error);
+      }
+    }
+  };
+  const handleGoogleSignIn = () => {};
+  return (
+    <Helmet title="Đăng nhập">
+      <div className="login">
+        <div className="login__name">Login</div>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form__group">
+            <input
+              id="emailInput"
+              type="email"
+              aria-required="true"
+              aria-label="email"
+              placeholder="Enter your email..."
+              onChange={(e) => setEmail(e.target.value)}
+              ref={refInputEmail}
+              required
+            />
+          </div>
+          <div className="form__group">
+            <input
+              id="passwordInput"
+              aria-required="true"
+              aria-label="password"
+              type="password"
+              placeholder="Enter your password..."
+              onChange={(e) => setPassword(e.target.value)}
+              ref={refInputPassword}
+              required
+            />
+          </div>
+          <Button size="sm">Đăng nhập</Button>
+          <div className="google__btn" onClick={handleGoogleSignIn}>
+            <GoogleButton type="dark" />
+          </div>
+        </form>
+        {error && <span className="error">{error}</span>}
+        <div className="login__regis">
+          Do not have an account? <Link to="/register">Create account</Link>
+        </div>
+      </div>
+    </Helmet>
+  );
+};
+export default React.memo(Login);
